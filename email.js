d831346df9b761135f2c4c30843ae70dc93fc81a
rodrigo <acs@srv539972.hstgr.cloud>
2025-08-28 15:30:20 -0300

    fix: ajusta portas no dockerfile

--- email.js
+++ email.js
@@ -46,7 +46,7 @@ app.post('/api/send-email', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3040
 app.listen(PORT, () => {
   console.log(`Servidor a correr na porta ${PORT}`)
 })
